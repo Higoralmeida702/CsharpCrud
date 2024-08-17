0001425add0a9b1e38bf8d67e5771d4a71e75a07
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-08-17 12:40:35 -0300

    Inicialização de projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CsharpCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
