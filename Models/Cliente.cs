960d3f91cfd8da82e9fe9d3841e1d2efe1854cc9
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-08-17 12:42:46 -0300

    Adiciona modelo de dados 'Cliente' com propriedades Id, Nome, Cpf e Telefone.

---
+++ Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace CsharpCrud.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Cpf { get; set; }
+
+        public String Telefone { get; set; }
+    }
+}
